e7aa0467e0d6b395754f834e3991d88c007c0f0a
bobrynja <119498240+bobrynja@users.noreply.github.com>
2022-11-30 14:28:40 +0300

    Add files via upload
    1 консольное приложение

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double S;
+            Console.WriteLine("Введите площадь поверхности куба:");
+            try
+            {
+                S = Convert.ToDouble(Console.ReadLine());
+                if (S <= 0)
+                {
+                    Console.WriteLine("Площадь не может быть неположительным числом");
+                }
+                else
+                {
+                    Console.WriteLine(Math.Round(Math.Sqrt(S / 6),3));
+                }
+            }
+            catch { Console.WriteLine("Некорректный ввод данных"); };
+            Console.ReadKey();
+        }
+    }
+}
